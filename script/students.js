db4c7c8023a0dda96b10fe3a4bae59e745b8010d
JadHajali1 <jadhajali773@gmail.com>
2025-04-15 23:10:39 +0300

    big O notation of student list

--- script/students.js
+++ script/students.js
@@ -5,31 +5,33 @@ const students = [
     { name: "Charlie", scores: [100, 100, 100] }
   ];
 
-
-let garde ; 
+//big O(n) => n being the lenght of the score
+let garde ; //big O (1)
 function generateReports(students) {
-    const sum = students.scores.reduce((sum, scores) => sum + score , 0);
-    const average = sum / scores.lenght;
+    const sum = students.scores.reduce((sum, scores) => sum + score , 0); //big O(n). becuase reduce method will iterates throgh the list of the score
+    const average = sum / scores.lenght; //big O (1)
     
 
-    if(average >= 90){
+    if(average >= 90){ //big O (1)
         grade = "A";
     }
-    else if(average >= 80){
+    else if(average >= 80){ //big O (1)
         grade = "B";
     }
-    else if(average >= 70){
+    else if(average >= 70){ //big O (1)
         grade = "C";
     }
-    else if(average >= 60){
+    else if(average >= 60){ //big O (1)
         grade = "D";
     }
-    else grade = "F";
+    else grade = "F"; //big O (1)
 
     return {
-        name: students.name,
-        average: average,
-        garde: grade
+        name: students.name, //big O (1)
+        average: average,   //big O (1)
+        garde: grade    //big O (1)
     };
 
 }
+
+console.log(generateReports(students)); //big O (1)
